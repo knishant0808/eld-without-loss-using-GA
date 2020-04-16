72b1b813d1d72fd407675fd47e23b3c71265b96e
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-16 23:19:10 +0530

    Update bin_to_dec.m

--- bin_to_dec.m
+++ bin_to_dec.m
@@ -1,5 +1,5 @@
 function dec_value = bin_to_dec(pop_data)
-multiplier = [2^-8 2^-7 2^-6 2^-5 2^-4 2^-3 2^-2 2^-1 1 2 4 8];
+multiplier = [8 4 2 1 2^-1 2^-2 2^-3 2^-4 2^-5 2^-6 2^-7 2^-8];
 dec_value = sum(pop_data.*multiplier, 1);
 end
 
