4caf3f8c086613bdfaf7a631e25e47e0c4b1f436
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-16 14:20:56 +0530

    Create sort_data.m

---
+++ sort_data.m
@@ -0,0 +1,7 @@
+function pop_data = sort_data(pop_data)
+[~, I] = sort(pop_data(:, 10, 'descend');  %Sorting based on fitness in decreasing order
+                                           %Getting the index of elements
+                                           %of the sorted array
+pop_data = pop_data(I, :);      
+end
+
