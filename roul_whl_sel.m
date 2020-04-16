f63c6881aee00a2f814ff938137cc12f3b607dfa
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-16 23:21:26 +0530

    Update roul_whl_sel.m

--- roul_whl_sel.m
+++ roul_whl_sel.m
@@ -1,7 +1,7 @@
 function [selection, idx] = roul_whl_sel(temp_data)
 num = rand;
 for i = 1:length(temp_data, 1)
-    if((num >= temp_data(i, 14) & (num < temp_data(i, 15)))
+    if((num >= temp_data(i, 18) & (num < temp_data(i, 19)))
         idx = i;
         selection = temp_data(i);
     end
