e76fb4184ae97f1690299f18de56ca602ab0e905
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-16 23:20:54 +0530

    Update crossover.m

--- crossover.m
+++ crossover.m
@@ -1,4 +1,5 @@
-function [pop_data,temp_data] = crossover(temp_data,c_prob, chrom_size)
+function [selected_pop, temp_data] = crossover(temp_data,c_prob, chrom_size)
+selected_pop = [];
 n_selection = length(temp_data, 1)*c_prob;
 for l = 1:n_selection
     [parent1, idx1] = roul_whl_sel(temp_data);
@@ -8,9 +9,9 @@ for l = 1:n_selection
     
     crossover_point = randi(chrom_size);
     
-    child1 = [parent1(1:crossover_point) parent2(crossover_point:8);
-    child2 = [parent2(1:crossover_point) parent1(crossover_point:8);
+    child1 = [parent1(1:crossover_point) parent2(crossover_point:12);
+    child2 = [parent2(1:crossover_point) parent1(crossover_point:12);
     
-    
+    selected_pop = [selected_pop; child1; child2];
 end
 
