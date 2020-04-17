2376e1ce447db4708727c5040afc483d23997477
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-17 23:27:15 +0530

    Update bin_to_dec.m

--- bin_to_dec.m
+++ bin_to_dec.m
@@ -1,6 +1,6 @@
-function dec_value = bin_to_dec(pop_data)
-multiplier = [8 4 2 1 2^-1 2^-2 2^-3 2^-4 2^-5 2^-6 2^-7 2^-8];
-binary_data = pop_data(:, 1:12);
+function dec_value = bin_to_dec(pop_data, chrom_size)
+multiplier = [8 4 2 1 2^-1 2^-2 2^-3 2^-4 2^-5 2^-6 2^-7 2^-8 2^-9 2^-10 2^11];
+binary_data = pop_data(:, 1:chrom_size);
 dec_value = binary_data*multiplier';
 end
 
