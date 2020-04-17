2e8daa2011cc7f89652360055be2856b2aca98eb
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-17 23:27:26 +0530

    Update crossover.m

--- crossover.m
+++ crossover.m
@@ -4,10 +4,10 @@ n_selection = size(temp_data, 1)*c_prob;
 for l = 1:n_selection
     child1 = [];
     child2 = [];
-    [parent1, idx1] = roul_whl_sel(temp_data);
-    temp_data = remov(temp_data, idx1);
-    [parent2, idx2] = roul_whl_sel(temp_data);
-    temp_data = remov(temp_data, idx2);
+    [parent1, idx1] = roul_whl_sel(temp_data, chrom_size);
+    temp_data = remov(temp_data, idx1, chrom_size);
+    [parent2, idx2] = roul_whl_sel(temp_data, chrom_size);
+    temp_data = remov(temp_data, idx2, chrom_size);
     
     crossover_point = randi([1,chrom_size-1]);
     
