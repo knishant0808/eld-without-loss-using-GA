b50b6a45704db1caba44e79e125c152fe5dde222
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2020-04-17 19:55:17 +0530

    Update calc_fitness.m

--- calc_fitness.m
+++ calc_fitness.m
@@ -8,7 +8,7 @@ for i = 1;length(lambda)
     end
 end
 power_gen = (lambda - cons.B)./(2*cons.A);
-tot_power = sum(power_gen, 1);
+tot_power = sum(power_gen, 2);
 error = abs(cons.Pd - tot_power);
 fitness = 1./(1 + error);
 cost = sum((power_gen.^2).*cons.A + (power_gen.*cons.B), 2);
